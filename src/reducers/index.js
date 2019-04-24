7060b1c40bf292439745f3a98afc3abc44f5576b
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-24 09:44:17 -0300

    Create react app structure

---
+++ src/reducers/index.js
@@ -0,0 +1,61 @@
+export const Genesis_Genelogies = [
+  
+]
+    Genealogias de Gênesis
+Data Anno Domini - AD (AC - Antes de Cristo)	Data Anno Mundi - AM	Evento 
+4006	0	Adão e Eva são criados 
+3876	130	Nascimento de Sete 
+3771	235	Nascimento do Enos 
+3681	325	Nascimento de Cainã 
+3611	395	Nascimento de Maalalel 
+3546	460	Nascimento de Jerede 
+3384	622	Nascimento de Enoque 
+3319	687	Nascimento de Matusalém
+3132	874	Nascimento de Lameque 
+3076	930	Morte de Adão
+2964	1042	Morte de Sete
+2954	1052	Enoque transladado por Deus
+2950	1056	Nascimento de Noé
+2866	1140	Morte de Enos
+2771	1235	Morte de Cainã 
+2716	1290 	Morte de Maalalel
+2584	1422 	Morte de Jarede 
+2448	1558	Nascimento de Sem
+2352	1654 	Morte de Lameque
+2350	1656	Morte de Matusalém 
+		DILÚVIO
+2348	1658	Nascimento de Arfaxade
+2313	1693	Nascimento de Salá
+2283	1723	Nascimento de Héber
+2249	1757	Nascimento de Pelegue
+2219	1787	Nascimento de Reú
+2187	1819	Nascimento de Serugue
+2157	1849	Nascimento de Naor
+2128	1878	Nascimento de Terá
+2058	1948	Nascimento de Abraão
+2068	1958 	Nascimento de Sarah 
+2010	1996	Morte de Pelegue 
+		Torre de Babel (?) 
+2009	1997	Morte de Naor 
+2000	2006 	Morte de Noé 
+1980	2026	Morte de Reú 
+1972	2034 	Nascimento de Ismael 
+1959	2047 	Destruição de Sodoma e Gomorra
+1958	 2048 	Nascimento de Isaque
+1957	2049 	Morte de Serugue
+1923	2083 	Morte de Tera
+1921	2085 	Morte de Sarah
+1910	2096 	Morte de Arfaxade
+1898	2108	Nascimento de Jacó e Esaú
+1883	2123	Morte de Abraão 
+1880	2126	Morte de Salá 
+1848	2158	Morte de Sem 
+1835	2171	Morte de Ismael 
+1819	2187	Morte de Héber 
+1807	2199	Nascimento de José 
+1778	2228	Morte de Isaque 
+1761	2245	Morte de Jacó
+1697	2309	Morte de José
+
+const Exodus_Genelogies = Genealogias do Êxodo
+1573	2433	Nascimento de Moisés
