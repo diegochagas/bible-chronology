a1a2f8f4b2c69dc650a8e3834b9dd112ff6a1146
Diego Rocha Chagas <drochachagas@gmail.com>
2019-04-24 15:10:51 -0300

    Create app component and index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import './App.css';
-import ExodusGenelogies from './components/ExodusGenelogies';
-import GenesisGenelogies from './components/GenesisGenelogies';
+import GenealogyTable from './components/GenealogyTable';
+import { getGenesisGenealogies, getExodusGenealogies } from './actions';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>A Cronologia Bíblica</h1>
-        <h2>A DATA DO DILÚVIO NA HISTÓRIA BÍBLICA</h2>
-      </header>
-      <ExodusGenelogies/>
-      <GenesisGenelogies/>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount(){
+    this.props.getGenesisGenealogies();
+    this.props.getExodusGenealogies();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>A Cronologia Bíblica</h1>
+          <h2>A data do dilúvio na história bíblica</h2>
+        </header>
+        <GenealogyTable tableTitle="Genealogias de Gênesis" genealogies={this.props.genesis} />
+        <GenealogyTable tableTitle="Genealogias do Êxodo" genealogies={this.props.exodus} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { genesis: state.genesis, exodus: state.exodus };
+}
+
+export default connect(mapStateToProps, {getGenesisGenealogies, getExodusGenealogies})(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStore(reducer)}>
+    <App />
+  </Provider>, 
+  document.getElementById('root'));
 
